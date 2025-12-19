c6f3274d42d7d591f37413519ebee17998c07c54
Valentina <72305060+vlagoraad@users.noreply.github.com>
2025-12-19 10:17:49 -0300

    feat: Account domain model with deposit and withdraw

---
+++ banking-api/src/main/java/com/ebanx/bankingapi/domain/Account.java
@@ -0,0 +1,25 @@
+public class Account {
+    private String id;
+    private BigDecimal balance;
+
+    public Account(String id, BigDecimal balance){
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public void deposit(BigDecimal amount){
+        balance = balance.add(amount);
+    }
+
+    public void withdraw(BigDecimal amount){
+        balance = balance.subtract(amount);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public BigDecimal getBalance(){
+        return balance;
+    }
+}
